61df1533ec0bf511762ec72b19aba994818e96a8
Patrick <jamespmcoleman@gmail.com>
2025-11-29 15:07:49 -0700

    Added random number functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+import random
+
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel 
-from typing import Optional
+from typing import Optional, Annotated
 
 app = FastAPI()
 
@@ -13,7 +15,45 @@ books = [
 class Book(BaseModel):
     title: str
     author: str
+
+
+@app.get("/")
+def home():
+    """Home endpoint returning a welcome message."""
+    return {"message": "Welcome the Randomizer API!"}
+
+@app.get("/random/{max_value}")
+def get_random_number(max_value: int):
+    """Generate a random number between 1 and max_value."""
+    return {
+        "max": max_value,
+        "random_number": random.randint(1, max_value)
+    }
     
+
+@app.get("/random-between")
+def get_random_number_between(
+    max_value: Annotated[int, Query(
+        title="Maximum Value",
+        description="The upper limit for the random number generation",
+        ge=1,
+        le=1000
+    )]= 99,
+    min_value: Annotated[int, Query(
+        title="Minimum Value",
+        description="The lower limit for the random number generation",
+        ge=1,
+        le=1000
+    )]= 1):
+    """Generate a random number between min_value and max_value."""
+    if min_value >= max_value:
+        raise HTTPException(status_code=400, detail="min_value must be less than max_value")
+    return {
+        "min": min_value,
+        "max": max_value,
+        "random_number": random.randint(min_value, max_value)
+    }
+
 
 @app.get("/books")
 def get_books(limit: Optional[int] = None):
@@ -23,7 +63,7 @@ def get_books(limit: Optional[int] = None):
     return {"books": books}
 
 
-@app.get("/books/{book_id}")
+@app.get("/book/{book_id}")
 def get_book(book_id: int):
     """Retrieve a book by its ID."""
     for book in books:
